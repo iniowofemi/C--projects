b448876c5dee644e1d19fb6e17f36de0ea443222
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2023-04-17 12:06:20 +0100

    Add files via upload

---
+++ codeacademy-projects/Console Creatures/Console Creatures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleCreatures
+{
+    class Program
+    {
+        static void Main()
+        {
+            //This program is to create a creature
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("This ghost's name is Herbert");
+        }
+    }
+}
