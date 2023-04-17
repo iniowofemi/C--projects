54435f10f6aeea228560281fa5b198fc83e9b704
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2023-04-17 12:02:29 +0100

    Add files via upload

---
+++ codeacademy-projects/Caesar Cipher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            Console.WriteLine("What message fo you wish to translate? ");
+            string msg = Console.ReadLine();
+            char[] secretMessage = msg.ToCharArray();
+
+            char[] encryptedArray = new char[secretMessage.Length];
+
+            for (int i = 0; i < secretMessage.Length; i++)
+            {
+                char letter = secretMessage[i];
+                int index = Array.IndexOf(alphabet, letter);
+                encryptedArray[i] = alphabet[(index + 3) % alphabet.Length];
+            }
+            string encryptedMessage = String.Join("", encryptedArray);
+            Console.WriteLine(encryptedMessage);
+        }
+    }
+}
