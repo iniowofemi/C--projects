4d06563c515649727d198a68bdabc1aec052d0cd
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2023-04-17 12:09:08 +0100

    Add files via upload

---
+++ codeacademy-projects/The Object of Your Affection/Profile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Profile
+    {
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        public Profile(
+        string name,
+        int age,
+        string city = "N/A",
+        string country = "N/A",
+        string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[0];
+        }
+
+        public string ViewProfile()
+        {
+            string info = $"Name: {name}\nAge: {age}\nCity: {city}\nCountry: {country}\nPronouns: {pronouns}";
+
+            if (this.hobbies.Length > 0)
+            {
+                info += "\nHobbies:\n";
+                foreach (string hobby in hobbies)
+                {
+                    info += $"- {hobby}\n";
+                }
+            }
+            return info;
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+    }
+}
---
+++ codeacademy-projects/The Object of Your Affection/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakilla", 30, "New York", "USA", "he/him");
+            sam.SetHobbies(new string[] { "Gaming", "Reading", "Cooking" });
+            Console.WriteLine(sam.ViewProfile());
+        }
+    }
+}
